af7939935eb08c6cd66f604d725eafee706f031b
Dmitry Demyanov <dmitryd20@gmail.com>
2022-02-16 21:44:10 +0300

    removed autogenerated code

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -3,24 +3,16 @@ import FluentPostgresDriver
 import Leaf
 import Vapor
 
-// configures your application
 public func configure(_ app: Application) throws {
-    // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
     app.databases.use(.postgres(
         hostname: Environment.get("DATABASE_HOST") ?? "localhost",
         port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database"
+        username: Environment.get("DATABASE_USERNAME") ?? "dmitry",
+        password: Environment.get("DATABASE_PASSWORD") ?? "",
+        database: Environment.get("DATABASE_NAME") ?? "xcbuildcollector"
     ), as: .psql)
 
-    app.migrations.add(CreateTodo())
-
     app.views.use(.leaf)
-
-    
 
     // register routes
     try routes(app)
